c1aa684511e23371a4b719e5170be1f0fd1a7d0e
Philip <philip@deranged.dk>
2021-09-04 18:52:00 +0200

    get postgress and make it accesible

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
+const { Pool } = require('pg')
+const port = 3000;
+
+const db = new Pool({
+  "user": "postgres",
+  "host": "localhost",
+  "database": "hytte_index",
+  "password": "postgres",
+  "port": 5432
+});
 
 app.get("/health", (req, res) => {
   res.send("Server is alive and well");
 });
 
-app.listen(3000,()=> {
-  console.log("backend listening on 3000");
+app.listen(port,()=> {
+  console.log(`backend listening on http://localhost:${port}`);
 });
